0c88890530b143e25a42a2e8126da559a9f2edcd
전정훈 <shu7081@gmail.com>
2026-06-19 04:49:35 +0900

    feat(react): add Stack layout component

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,3 +4,5 @@ export { Button } from "./Button/Button";
 export type { ButtonProps, ButtonVariant, ButtonSize } from "./Button/Button";
 export { TextField } from "./TextField/TextField";
 export type { TextFieldProps } from "./TextField/TextField";
+export { Stack } from "./Stack/Stack";
+export type { StackProps, SpacingScale } from "./Stack/Stack";
